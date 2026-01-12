023994cd8276043a846a8fe9496cda73c0ed04e8
zpect <95605545+zpect-dev@users.noreply.github.com>
2026-01-12 10:31:01 -0400

    Cambios en el nombre general de la app

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -6,7 +6,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Negociaciones';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -3,7 +3,7 @@ import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import ReactDOMServer from 'react-dom/server';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Negociaciones';
 
 createServer((page) =>
     createInertiaApp({
